02cc6ff7bc62f63f424a6699c334dbde9f1b26e4
aaguerra <alexis_17_91@hotmail.es>
2016-06-11 23:49:12 -0500

    sfsadfi

--- app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
+++ app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
@@ -1,26 +1,43 @@
 package com.yachai.reto.prueba1;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button b1,b2;
+    public LocationListener mLocationListener;
+    Button b1, b;
     ImageView iv;
+    public TextView coordenadas;
+
+
+    public LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=(Button)findViewById(R.id.button);
-        iv=(ImageView)findViewById(R.id.imageView);
+        b1 = (Button) findViewById(R.id.button);
+        iv = (ImageView) findViewById(R.id.imageView);
+        coordenadas = (TextView) findViewById(R.id.textView_coordenadas);
+        b = (Button) findViewById(R.id.button_posicion);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +46,86 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+
+        mLocationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                System.out.println("-----------11");
+                coordenadas.clearComposingText();
+                coordenadas.append(" " + location.getLongitude() + " " + location.getLatitude());
+                endGPS();
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+
+
+        };
+/*
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+*/
+
+        configure_button();
     }
+
+    /**
+     * @return the last know best location
+     *
+    private Location getLastBestLocation() {
+    Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+    long GPSLocationTime = 0;
+    if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+    long NetLocationTime = 0;
+
+    if (null != locationNet) {
+    NetLocationTime = locationNet.getTime();
+    }
+
+    if ( 0 < GPSLocationTime - NetLocationTime ) {
+    return locationGPS;
+    }
+    else {
+    return locationNet;
+    }
+    }
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO Auto-generated method stub
@@ -38,5 +134,52 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bp = (Bitmap) data.getExtras().get("data");
         iv.setImageBitmap(bp);
     }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        System.out.println("-----------1");
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+
+                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+                //if (null != locationGPS) { 3 = locationGPS.getTime(); }
+
+            }
+        });
+    }
+
+
+    public void endGPS() {
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mLocationManager.removeUpdates(mLocationListener);
+            //lmanager=null;
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 
 }
