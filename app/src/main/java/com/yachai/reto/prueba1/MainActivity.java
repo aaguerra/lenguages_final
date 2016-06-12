e915b521664e3fb33a5ad1ea1c4fc6d344c2426b
Joel <joelerll@gmail.com>
2016-06-12 03:44:58 -0500

    Hecho: manifest, gradle, string

--- app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
+++ app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
