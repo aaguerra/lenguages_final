f86497db6a9a83273f832cd67d6823cce7a774e8
Joel <joelerll@gmail.com>
2016-06-13 20:22:03 -0500

    borrando cosas  inecesarias

--- app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
+++ app/src/main/java/com/yachai/reto/prueba1/MainActivity.java
@@ -81,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
         };
 /*
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
                 LOCATION_REFRESH_DISTANCE, mLocationListener);
 */
